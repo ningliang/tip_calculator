26ef079de979ac5d41c674d144967d7e6ef09051
Ning Liang <ning.q.liang@gmail.com>
2014-02-17 23:03:01 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Ning Liang on 2/17/14.
+//  Copyright (c) 2014 Ning Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
